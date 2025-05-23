4c5e2820195dbf09635ec88805d8931f7cf820dc
amichaeel <mail@anthonymham.com>
2025-05-22 19:13:52 -0700

    FIX: build error unused link

--- app/(main)/profile/page.tsx
+++ app/(main)/profile/page.tsx
@@ -1,7 +1,6 @@
 import { getAuthSession } from "@/lib/auth";
 import { redirect } from "next/navigation";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import Link from "next/link";
 import { Button } from "@/components/ui/button";
 
 export default async function ProfilePage() {
